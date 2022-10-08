b371a97073ee306fe27d60d1fc8f75111c2ca5a9
tekba <tekba@DESKTOP-46QGCML>
2022-10-08 17:41:39 +0200

    Agregar archivos de proyecto.

---
+++ Gato5/Gato5.py
@@ -0,0 +1,102 @@
+"""print("Te escribiremos que edad tienes segun tu edad escribe tu edad para comprobar")
+
+Gato=float(input("Escribe tu edad "))
+if Gato>=0 and Gato<=9:
+   print("Estas en la infancia")
+if Gato>=10 and Gato<=12:
+   print("Estas en la preadolescencia")
+if Gato>=13 and Gato<=17:
+   print("Estas en la adolescencia")
+if Gato>=18 and Gato<=24:
+   print("Estas en la juventud")
+if Gato>=25 and Gato<=59:
+   print("Estas en la adultez")
+if Gato>=60:
+   print("Estas en la vejez")"""
+"""
+i=1
+while i<=100:
+    print(i)
+ 
+ "2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97"
+ """
+"""
+Numeros=[42,2,5,7,123,884,75,37,139,11,342,446,33,77,55,73]
+Pato=int(input("Escribe un numero para ganar un premio"))
+print("Enhorabuena has ganado un premio")
+if Numeros[1]:
+    print("Has encontrado un premio especial")
+if Numeros[9]:
+    print("Lo has logrado has encontrado un premio especial")
+if Numeros[13]:
+    print("WOW un premio especial felicidades")
+    """
+"""
+x=[0,1,0,0,0,0,0,0,1,1,1,0,1,0,1]
+
+Numero_1=0
+
+for t in x:
+    if t==1:
+       Numero_1+=1
+       
+print("Han pagado "+str(Numero_1) )
+"""
+"""
+Deb=[1,5,6,7,9,3,6,7,2,3]
+
+t=0
+
+for S in Deb:
+    if S==2:
+        t*=2
+    elif S==3:
+        t-=3
+    else:
+        t+=S
+print("El resultado es " + str(t))
+"""
+"""
+thislist =[1,2,3,4,5,6,7,8,9,10]
+thislist.append (0)
+while =
+print([])
+"""
+Hacha=False
+Cuerda=False
+telefono=False
+print("Bienvenido a el juego para llegar sano y salvo a tu casa")
+print("Empezaremos por donde vas a ir")
+print("Elige 1 o 2 ")
+Gato=int(input())
+if Gato==1:
+    print("Has escogido el numero 1 apareceras en la casa de tu tia" )
+elif Gato==2:
+     print("Has escogido el numero 2 apareceras en un ciudad")
+print("Vaya has aparecido en ese lugar ten te puedo dar 3 objetos pero solo puedes escoger 1 para tu viaje ")
+print("Si quieres una Hacha escoge 1 si quieres una Cuerda escoge 2 si quieres un telefono escoge 3")
+Objeto=input()
+if  Objeto==1:
+    print("Has escogido la Hacha buena eleccion ")
+elif Objeto==2:
+     print("Has escogido la Cuerda, good ") 
+     Hacha=True
+elif Objeto==3:
+     print("Has escogido el telefono, ten cuidado con la bateria ")
+print("Ahora que ya estamos listos vamonos")
+print("2 HORAS DESPUES")
+print("Ya se esta haciendo tarde, deberias salir ya de ahi y buscar tu casa. Pero ves el parque a tu izquierda y un bar a la derecha, Que vas a elegir? ")
+print("Escoge 1 para pasar por el parque o escoge 2 para pasar por el bar.")
+Refugio=input()
+if Refugio==1:
+   print("Has escogido pasar por el parque, creo que es mas seguro. ")
+elif Refugio==2:
+     print("Has escogido pasar por el bar, vamos a probar haber si nos pueden ayudar, pero no te metas en lios. ")
+print("Continuara...")
+print("tuturuturuuuuu")
+"""
+if ==True:
+    print("") 
+   Kitten=input()
+    if Kitten==1:
+       print("")"""
